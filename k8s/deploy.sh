f1b34d003efa0b369d65290530d7c3c7362aa3ab
rajivsiddiqui <rajivsiddiqui@gmail.com>
2025-06-08 23:29:20 +0600

    --validate=false add it in deploy.sh file

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-kubectl apply -f k8s/deployment.yml
-kubectl apply -f k8s/service.yml
+kubectl apply -f k8s/deployment.yml --validate=false
+kubectl apply -f k8s/service.yml --validate=false
