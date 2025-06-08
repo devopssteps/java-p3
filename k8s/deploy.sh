0025d02023829cccdd9e6bd4de17633400e610f6
rajivsiddiqui <rajivsiddiqui@gmail.com>
2025-06-09 00:01:08 +0600

    deploy.sh file

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-kubectl apply -f k8s/deployment.yaml 
-kubectl apply -f k8s/service.yaml 
+#kubectl apply -f k8s/deployment.yaml 
+#kubectl apply -f k8s/service.yaml 
+kubectl apply -f k8s/deployment.yaml --validate=false
+kubectl apply -f k8s/service.yaml --validate=false
+
