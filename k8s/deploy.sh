43bf11739867ef39e3dcbee8f356414d7543497f
rajivsiddiqui <rajivsiddiqui@gmail.com>
2025-06-09 17:01:19 +0600

    6

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 kubectl apply -f k8s/deployment.yaml 
 kubectl apply -f k8s/service.yaml 
-#kubectl apply -f k8s/deployment.yaml --validate=false
-#kubectl apply -f k8s/service.yaml --validate=false
+
 
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from DevOps CI/CD Pipeline 5";
+        return "Hello from DevOps CI/CD Pipeline 6";
     }
 }
 
